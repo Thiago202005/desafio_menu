39c7a92358d23e7d0655427c8feb5e32da165667
cogliattifede <federico.cogliatti@correo.ucu.edu.uy>
2024-08-21 09:22:36 -0300

    Implementación de la clase Dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -5,5 +5,27 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Dish
 {
+    private string _name;
+    public string Name
+    {
+        get { return _name; } set { _name = value; }
+    }
     
+    private float _price;
+    public float Price
+    {
+        get { return _price; } set { _price = value; }
+    }
+    
+    private bool _isVegetarian;
+    public bool IsVegetarian
+    {
+        get { return _isVegetarian;} set { _isVegetarian = value; }
+    }
+    private Dish(string name, float price, bool isVegetarian)
+    {
+        this._name = name;
+        this._price = price;
+        this._isVegetarian = isVegetarian;
+    }
 }
