ad3acce992fad0f47922bb851bad69971687a4a2
Natu <nf231105@gmail.com>
2024-08-21 10:10:06 -0300

    menu

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Ucu.Poo.Restaurant;
 
 using System.Collections;
@@ -6,7 +8,35 @@ using System.Collections;
 /// Representa el conjunto de platillos <see cref="Dish"/> disponibles en el
 /// restaurante.
 /// </summary>
+
+
+
+
 public class Menu
 {
     private ArrayList dishes = new ArrayList();
+
+    public void AddDish(Dish Newdish)
+    {
+        dishes.Add(Newdish);
+    }
+
+    public void RemoveDish(Dish deleteableDish)
+    {
+        dishes.Remove(deleteableDish);
+    }
+
+    public string GetDishByName(string name)
+    {
+        if (dishes.Contains(name))
+        {
+            return ($"{name} esta dentro del menu");// en todas las clases en todos los casos debe haber un return, Console.Writeln no sirve
+        }
+        else
+        {
+            return null;
+        }
+
+    }
 }
+    
